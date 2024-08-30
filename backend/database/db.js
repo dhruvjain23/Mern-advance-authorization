30dbd2ac6c7fc0bc335084524018ab10feda904c
Dhruv Jain <119651242+dhruvjain23@users.noreply.github.com>
2024-08-30 16:37:50 +0530

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export const dbconnect = async()=>{
     try{
-    await mongoose.connect('mongodb+srv://djain01d:OGtHfCAqzMheYs7k@cluster0.gy2tx.mongodb.net/auth_db?').then(()=>console.log('DB Connected'))}
+    await mongoose.connect('your_Mongo DB URL'))}
     catch (error)
     {
         console.log("Error connection to Database",error.message);
